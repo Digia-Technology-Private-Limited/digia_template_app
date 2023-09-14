a8835b82bfe8c313afaa0e202e45ac6501dc5002
ch1nru5t <chinrust@gmail.com>
2023-09-14 20:32:11 +0530

    added device_preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,18 @@
-import 'package:digia_ui/Utils/digia_ui_sdk.dart';
-import 'package:digia_ui/Utils/config_resolver.dart';
-import 'package:digia_ui/Utils/util_functions.dart';
-import 'package:digia_ui/core/page/dui_page.dart';
-import 'package:digia_ui/core/pref/pref_util.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:http/http.dart' as http;
 import 'dart:async';
+
+import 'package:device_preview/device_preview.dart';
+import 'package:digia_ui/Utils/config_resolver.dart';
+import 'package:digia_ui/Utils/digia_ui_sdk.dart';
+import 'package:digia_ui/core/page/dui_page.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 FutureOr<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // Load configuration
-  final response = await http
-      .get(Uri.parse('https://demo1139397.mockable.io/bytes'));
+  final response =
+      await http.get(Uri.parse('https://demo1139397.mockable.io/bytes'));
   await DigiaUiSDk.initialize(response.body);
   runApp(const MyApp());
 }
@@ -25,7 +25,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Digia Template App',
-      home: DUIPage(initData: ConfigResolver().getfirstPageData()),
+      home: DevicePreview(
+        enabled: defaultTargetPlatform == TargetPlatform.windows ||
+            defaultTargetPlatform == TargetPlatform.macOS ||
+            defaultTargetPlatform == TargetPlatform.linux ||
+            defaultTargetPlatform == TargetPlatform.fuchsia,
+        builder: (context) => DUIPage(
+          initData: ConfigResolver().getfirstPageData(),
+        ),
+      ),
     );
   }
 }
