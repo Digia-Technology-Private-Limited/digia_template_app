979d29b423f829161c412a8d4e0eb3b8f0a696b3
Premansh Tomar <premanshtomar@gmail.com>
2023-11-24 12:04:25 +0530

    fixed a bug in not navigating screens in Device Preview screen

--- lib/main.dart
+++ lib/main.dart
@@ -4,27 +4,38 @@ import 'dart:convert';
 import 'package:device_preview/device_preview.dart';
 import 'package:digia_ui/Utils/config_resolver.dart';
 import 'package:digia_ui/Utils/digia_ui_sdk.dart';
-import 'package:digia_ui/Utils/util_functions.dart';
 import 'package:digia_ui/core/page/dui_page.dart';
 import 'package:digia_ui/core/page/dui_page_bloc.dart';
 import 'package:digia_ui/core/page/dui_page_event.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:http/http.dart' as http;
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:http/http.dart' as http;
+
 FutureOr<void> main() async {
   // print(const String.fromEnvironment('projectId'));
   WidgetsFlutterBinding.ensureInitialized();
   // Load configuration
-  final response =
-      await http.post(Uri.parse('https://app.digia.tech/hydrator/api/config/getAppConfig'),
-          body: {
-        "projectId": const String.fromEnvironment('projectId')
-      });
-  final apiDataResponse = await jsonDecode(response.body as String);
-  await DigiaUiSDk.initializeByJson(apiDataResponse['data']['response']);
-  runApp(const MyApp());
+  final response = await http.post(
+    Uri.parse('https://app.digia.tech/hydrator/api/config/getAppConfig'),
+    body: {
+      "projectId": const String.fromEnvironment('projectId'),
+    },
+  );
+  final apiDataResponse = await jsonDecode(response.body);
+  await DigiaUiSDk.initializeByJson(
+    apiDataResponse['data']['response'],
+  );
+  runApp(
+    DevicePreview(
+      enabled: defaultTargetPlatform == TargetPlatform.windows ||
+          defaultTargetPlatform == TargetPlatform.macOS ||
+          defaultTargetPlatform == TargetPlatform.linux ||
+          defaultTargetPlatform == TargetPlatform.fuchsia,
+      builder: (context) => const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -40,37 +51,35 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: Colors.white,
         primaryColor: Colors.blueAccent,
         brightness: Brightness.light,
-        appBarTheme: AppBarTheme(
+        appBarTheme: const AppBarTheme(
           elevation: 0,
           iconTheme: IconThemeData(
             color: Colors.blueAccent,
           ),
           color: Colors.blueAccent,
-          systemOverlayStyle: const SystemUiOverlayStyle(
+          systemOverlayStyle: SystemUiOverlayStyle(
             statusBarColor: Colors.transparent,
             statusBarBrightness: Brightness.light,
             statusBarIconBrightness: Brightness.light,
           ),
           titleTextStyle: TextStyle(
-              color: Colors.black,
-              fontSize: 20,
-              fontWeight: FontWeight.w700),
+            color: Colors.black,
+            fontSize: 20,
+            fontWeight: FontWeight.w700,
+          ),
         ),
       ),
-      home: DevicePreview(
-        enabled: defaultTargetPlatform == TargetPlatform.windows ||
-            defaultTargetPlatform == TargetPlatform.macOS ||
-            defaultTargetPlatform == TargetPlatform.linux ||
-            defaultTargetPlatform == TargetPlatform.fuchsia,
-        builder: (context) =>  BlocProvider(
-          create: (context) {
-            final resolver = ConfigResolver();
-            return DUIPageBloc(
-                initData: resolver.getfirstPageData(), resolver: resolver)
-              ..add(InitPageEvent());
-          },
-          child: const DUIPage(),
-        ),
+      home: BlocProvider(
+        create: (context) {
+          final resolver = ConfigResolver();
+          return DUIPageBloc(
+            initData: resolver.getfirstPageData(),
+            resolver: resolver,
+          )..add(
+              InitPageEvent(),
+            );
+        },
+        child: const DUIPage(),
       ),
     );
   }
