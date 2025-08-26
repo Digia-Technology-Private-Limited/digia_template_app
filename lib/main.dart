c524c35e61b6cb5f763e4094f274dcb77a1681d0
Vivek Singh <viveksingh@digia.tech>
2025-08-26 15:15:05 +0530

    Updated repo to latest configs

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,22 @@
 import 'package:digia_ui/digia_ui.dart';
-
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final digiaUI = await DigiaUI.initialize(
+    DigiaUIOptions(
+      accessKey: const String.fromEnvironment('projectId'),
+      flavor: Flavor.debug(environment: Environment.production)
+    ),
+  );
 
-  @override
-  Widget build(BuildContext context) {
-    return DUIApp(
-      digiaAccessKey: '67d25ecc68d0b85479225509',
-      flavorInfo: Debug(null),
-      environment: 'development',
-      baseUrl: 'https://app.digia.tech/api/v1',
-      networkConfiguration:
-          NetworkConfiguration(defaultHeaders: {}, timeout: 30),
-    );
-  }
+runApp(
+    DigiaUIApp(
+      digiaUI: digiaUI,
+      builder: (context) => MaterialApp(
+        home: DUIFactory().createInitialPage(),
+      ),
+    ),
+  );
 }
